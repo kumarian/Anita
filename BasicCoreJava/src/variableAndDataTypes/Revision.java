51c09665c94bea9b4f3086393b57221bcbf8f4a1
kumarian <anita.sonu18@gmail.com>
2019-03-02 16:47:15 +0530

    Commiting first time

---
+++ BasicCoreJava/src/variableAndDataTypes/Revision.java
@@ -0,0 +1,37 @@
+package variableAndDataTypes;
+
+public class Revision {
+
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+			
+		}
+		
+int a=10;
+int b=20;
+if(a>b)
+{
+	System.out.println("a is greatest");
+}else
+{
+{System.out.println("b is greatest");
+	}
+
+// Array
+String[]str=new String[3];
+str[0]="Anand";
+str[1]="Bhaire";
+str[2]="Techbodhi1";
+}
+//10,true and Anand
+Object[]obj=new Object[3];
+obj[0]=10;
+obj[1]=true;
+obj[2]="Anand";
+System.out.println(obj[1]);
+	}
+}
+
